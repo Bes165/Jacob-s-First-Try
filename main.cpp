1f9b6bff441b83403deb73ad2ae84eae1f98a310
JacobLiuDuck <jacobliu001@qq.com>
2017-10-18 14:37:07 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//main.cpp by JacobLiu
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i >= 1; i += 0) {
+        printf ("I have a pen!\n");
+        printf ("I have an apple!\n");
+        printf ("Oh!\n");
+        printf ("I have a applepen!\n");
+        printf ("I have an applepen!\n");
+        printf ("I have an pen!\n");
+        printf ("Oh!\n");
+        printf ("I have a pineapplepen!\n");
+    }
+    return 0;
+}
